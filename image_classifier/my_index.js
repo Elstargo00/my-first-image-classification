2e382c6a130dfd9ca5d7049494d15b97a531d064
Elstargo00 <53881261+Elstargo00@users.noreply.github.com>
2019-08-31 00:47:01 +0200

    Add files via upload

---
+++ image_classifier/my_index.js
@@ -0,0 +1,30 @@
+const img_output =  document.getElementById('img-output');
+
+function loadFile(event){
+  img_output.src = URL.createObjectURL(event.target.files[0]);
+}
+
+async function classifyImg(){
+  console.log("Classify in progress...");
+  const result = await net.classify(img_output);
+  console.log("Classification completed");
+  document.getElementById('answer').innerText = `
+    Prediction: \n${result[0].className}
+  `
+}
+
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Sucessfully loaded model');
+
+  // Make a prediction through the model on our image.
+  //const imgEl = document.getElementById('img-output');
+  //const result = await net.classify(imgEl);
+  //console.log(result);
+  document.getElementById('classify').addEventListener("click", classifyImg);
+}
+
+app();
